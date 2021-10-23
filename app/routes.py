b1d060ba55cded9643984da173e5214a512d032e
githubli1123 <you@2068286164@qq.com>
2021-10-23 16:47:55 +0800

    template finished

--- app/routes.py
+++ app/routes.py
@@ -5,13 +5,30 @@
 # @Software:PyCharm
 
 from app import app  # 从app包中导入 app 这个实例
+from flask import render_template
 
 # 2个路由
 @app.route('/')
 @app.route('/index')
 # 1个视图函数
 def index():
-	return "Hello,World!"   # 返回一个字符串
+	# user = {'username': 'ZhouJie Wu'}
+	# return render_template('index.html', title='Home', user=user)    # 返回一个字符串
+	user = {'username': 'Miguel'}  # 用户
+	posts = [  # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in Portland!'
+		},
+		{
+			'author': {'username': 'Susan'},
+			'body': 'The Avengers movie was so cool!'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+
 
 
 
